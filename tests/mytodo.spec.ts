5bf656ed5bbbab0b25821d8dc1485955362b794e
ioiparzival <opshytoshnikita@gmail.com>
2024-02-19 18:43:13 +0200

    1

--- tests/mytodo.spec.ts
+++ tests/mytodo.spec.ts
@@ -1,18 +1,55 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, type Page } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc')
 })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+const todoItems = ['hello', 'just for testing', 'blablabla']
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click()
+test.describe('Create new TODO', () => {
+  test('can create a todo list', async ({ page }) => {
+    const newToDo = page.getByPlaceholder('What needs to be done?')
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
+    await newToDo.fill(todoItems[0])
+    await newToDo.press('Enter')
+
+    await expect(page.getByTestId('todo-title')).toHaveText([todoItems[0]])
+
+    await newToDo.fill(todoItems[1])
+    await newToDo.press('Enter')
+
+    await expect(page.getByTestId('todo-title')).toHaveText([todoItems[0], todoItems[1]])
+
+    await checkNumberOfTodosInLocalStorage(page, 2)
+  })
 })
+
+async function createDefaultTodos(page: Page) {
+  // create a new todo locator
+  const newTodo = page.getByPlaceholder('What needs to be done?')
+
+  for (const item of todoItems) {
+    await newTodo.fill(item)
+    await newTodo.press('Enter')
+  }
+}
+
+async function checkNumberOfTodosInLocalStorage(page: Page, expected: number) {
+  return await page.waitForFunction((e) => {
+    return JSON.parse(localStorage['react-todos']).length === e
+  }, expected)
+}
+
+async function checkNumberOfCompletedTodosInLocalStorage(page: Page, expected: number) {
+  return await page.waitForFunction((e) => {
+    return JSON.parse(localStorage['react-todos']).filter((todo: any) => todo.completed).length === e
+  }, expected)
+}
+
+async function checkTodosInLocalStorage(page: Page, title: string) {
+  return await page.waitForFunction((t) => {
+    return JSON.parse(localStorage['react-todos'])
+      .map((todo: any) => todo.title)
+      .includes(t)
+  }, title)
+}
